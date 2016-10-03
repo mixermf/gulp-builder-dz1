6be87d63e9f32b1ef5c2ac08bdc37f21063eabb2
Max Nik <myworkakk@gmail.com>
2016-10-03 09:07:33 +0300

    update

--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -4,9 +4,10 @@ module.exports = () => {
     $.gulp.task('sprite', () =>{
         var spriteData = $.gulp.src('./source/images/icons/*.png')
             .pipe($.gp.spritesmith({
+                padding: 20,
                 imgName: 'sprite.png',
-                imgPath: '../img/sprite.png',
-                cssName: 'sprite.scss'
+                //imgPath: '../img/sprites/sprite.png',
+                cssName: 'sprite.scss',
             }));
     return spriteData.img.pipe($.gulp.dest('source/images/sprites/'));
     return spriteData.css.pipe($.gulp.dest('source/style/'));
