74d1aaae8c2ccb1debb2aed8b62f2ee96aa2a2df
Max Nik <myworkakk@gmail.com>
2016-10-04 00:49:14 +0300

    autorize add(without radio)

--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -6,11 +6,11 @@ module.exports = () => {
             .pipe($.gp.spritesmith({
                 padding: 20,
                 imgName: 'sprite.png',
-                //imgPath: '../img/sprites/sprite.png',
+                imgPath: '../img/sprites/sprite.png',
                 cssName: 'sprite.scss',
             }));
-    return spriteData.img.pipe($.gulp.dest('source/images/sprites/'));
-    return spriteData.css.pipe($.gulp.dest('source/style/'));
+     return spriteData.img.pipe($.gulp.dest('source/images/sprites/'));
+     return spriteData.css.pipe($.gulp.dest('source/style/'));
 
     });
 };
